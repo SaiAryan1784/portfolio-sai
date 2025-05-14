7727dd704c6239acba2af24aa39fd88ed32b3cce
SaiAryan1784 <saiaryan.goswami1784@gmail.com>
2025-05-14 15:04:10 +0530

    [UI]: favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,20 @@ import "./globals.css";
 import DynamicTitleHandler from "./components/DynamicTitleHandler";
 import Navbar from "./(sections)/Navbar";
 import Goku from "./components/Goku";
+// No need to import favicon from public directory
 
 export const metadata: Metadata = {
   title: "Sai's Portfolio",
   description: "Created by Sai Aryan Goswami",
+  icons: {
+    icon: [
+      { rel: 'icon', url: '/favicon.ico', sizes: 'any' },
+    ],
+    shortcut: [{ url: '/favicon.ico' }],
+    apple: [
+      { url: '/luffydp.jpg' },
+    ],
+  },
 };
 
 export default function RootLayout({
